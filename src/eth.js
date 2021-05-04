c97a58488876eff32a57685acb681406f7a0027a
achraf <achraf.sellami@consensys.net>
2021-05-04 14:05:43 +0100

    refactor low level apis quorum.js

---
+++ src/eth.js
@@ -0,0 +1,41 @@
+function Eth(web3) {
+  web3.eth.extend({
+    methods: [
+      {
+        name: "fillTransaction",
+        call: "eth_fillTransaction",
+        params: 1,
+        inputFormatter: [web3.extend.formatters.inputTransactionFormatter],
+      },
+      {
+        name: "storageRoot",
+        call: "eth_storageRoot",
+        params: 2,
+        inputFormatter: [
+          web3.extend.formatters.inputAddressFormatter,
+          web3.extend.formatters.inputDefaultBlockNumberFormatter,
+        ],
+      },
+      {
+        name: "getQuorumPayload",
+        call: "eth_getQuorumPayload",
+        params: 1,
+      },
+      {
+        name: "sendTransactionAsync",
+        call: "eth_sendTransactionAsync",
+        params: 1,
+        inputFormatter: [web3.extend.formatters.inputTransactionFormatter],
+      },
+      {
+        name: "getContractPrivacyMetadata",
+        call: "eth_getContractPrivacyMetadata",
+        params: 1,
+        inputFormatter: [web3.extend.formatters.inputAddressFormatter],
+      },
+    ],
+  });
+  return web3;
+}
+
+module.exports = Eth;
