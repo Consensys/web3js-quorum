b2defc04a90f6f33699689b0c55d8117a4ca1f0d
achraf <achraf.sellami@consensys.net>
2022-01-11 19:40:12 +0400

    fix linting

--- 7nodes-test/deployContractViaHttp-pe.js
+++ 7nodes-test/deployContractViaHttp-pe.js
@@ -89,10 +89,13 @@ const bytecodeWithInitParam = simpleContract
       from: signAcct,
       isPrivate: true,
       privateFrom: "BULeR8JyUWhiuuCMU/HLA0Q5pzkYT+cHII3ZKBey3Bo=",
-      privateFor: ["QfeDAys9MPDs2XHExtc84jKGHxZg/aj52DTh0vtA3Xc=", "BULeR8JyUWhiuuCMU/HLA0Q5pzkYT+cHII3ZKBey3Bo="],
+      privateFor: [
+        "QfeDAys9MPDs2XHExtc84jKGHxZg/aj52DTh0vtA3Xc=",
+        "BULeR8JyUWhiuuCMU/HLA0Q5pzkYT+cHII3ZKBey3Bo=",
+      ],
       nonce: txCount,
       privacyFlag: 2,
-      mandatoryFor: ["QfeDAys9MPDs2XHExtc84jKGHxZg/aj52DTh0vtA3Xc="]
+      mandatoryFor: ["QfeDAys9MPDs2XHExtc84jKGHxZg/aj52DTh0vtA3Xc="],
     });
 
     console.log("Contract address: ", tx.contractAddress);
